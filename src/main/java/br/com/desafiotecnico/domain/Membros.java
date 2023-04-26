82b8aeb59cbe2b63ff550d1286f490a896356a65
Guilherme Rocha <guilhermerochadev@gmail.com>
2023-04-26 13:49:02 -0300

    Criação da classe domain entity Membros

---
+++ src/main/java/br/com/desafiotecnico/domain/Membros.java
@@ -0,0 +1,30 @@
+package br.com.desafiotecnico.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity(name="Membros")
+@Table(name = "membros")
+@Data
+public class Membros implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+
+    @JsonIgnore
+    @OneToOne
+    @JoinColumn(name="idprojeto")
+    @MapsId
+    private Projeto projeto;
+
+    @JsonIgnore
+    @OneToOne
+    @JoinColumn(name="idpessoa")
+    @MapsId
+    private Pessoa pessoa;
+}
